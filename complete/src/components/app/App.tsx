cf9af1a3aa0e2b489ad8174beab5352d4aeda9db
Brian Link <cpsubrian@gmail.com>
2018-10-03 21:52:55 -0700

    Ditch the header and dev size.

--- complete/src/components/app/App.tsx
+++ complete/src/components/app/App.tsx
@@ -10,13 +10,11 @@ export class App extends React.Component {
       <div className="app">
         <div className="background" />
         <div className="main">
-          <header>
+          <header style={{width: MAP_WIDTH}}>
             <h1>React Redux Zelda</h1>
+            <span>{process.env.NODE_ENV === 'development' ? 'development' : 'production'}</span>
           </header>
           <Map cellSize={CELL_SIZE} width={MAP_WIDTH} height={MAP_HEIGHT} />
-          <footer>
-            {process.env.NODE_ENV === 'development' ? <span>[ development ]</span> : null}
-          </footer>
         </div>
         <Toolbar />
       </div>
--- complete/src/constants.ts
+++ complete/src/constants.ts
@@ -1,6 +1,6 @@
 import {LayerName} from './types';
 
 export const CELL_SIZE = 16;
-export const MAP_WIDTH = CELL_SIZE * (process.env.NODE_ENV === 'production' ? 48 : 16);
-export const MAP_HEIGHT = CELL_SIZE * (process.env.NODE_ENV === 'production' ? 24 : 16);
+export const MAP_WIDTH = CELL_SIZE * 48;
+export const MAP_HEIGHT = CELL_SIZE * 24;
 export const LAYERS: Array<LayerName> = ['terrain', 'objects'];
