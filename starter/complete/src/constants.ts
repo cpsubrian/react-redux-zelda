f760fd9ab357bd629df25feb54d75f68a69243a4
Brian Link <cpsubrian@gmail.com>
2018-10-04 15:45:58 -0700

    Fixes starter director.

--- starter/complete/src/constants.ts
+++
@@ -1,6 +0,0 @@
-import {LayerName} from './types';
-
-export const CELL_SIZE = 16;
-export const MAP_WIDTH = CELL_SIZE * 48;
-export const MAP_HEIGHT = CELL_SIZE * 24;
-export const LAYERS: Array<LayerName> = ['terrain', 'objects'];
--- starter/complete/src/lib/idgen.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Generates simple random strings based on
- * Math.random.
- */
-export const idgen = () => {
-  return `${Math.random()}`.substr(2);
-};
