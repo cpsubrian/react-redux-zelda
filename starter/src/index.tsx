ed97f9ae2d86d3d2b5f40e69406e82d1fbcbdbbe
Brian Link <cpsubrian@gmail.com>
2018-10-09 08:40:51 -0700

    Tweaks to match docs.

--- starter/src/index.tsx
+++ starter/src/index.tsx
@@ -6,6 +6,5 @@ import {App} from './components/app/App';
 // Import CSS. With Webpack, this is how you include stylesheets in the build.
 import './index.css';
 
-// Render our <App /> component into the container. We wrap our app in
-// a redux provider, passing it the store.
+// Render our <App /> component into the container.
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
